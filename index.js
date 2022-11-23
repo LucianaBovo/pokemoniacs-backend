9961092218e4e51ad87192190f80a3b48d54d7f1
Luciana Margoto <margotoluciana@gmail>
2022-11-23 19:02:53 +0100

    port

--- index.js
+++ index.js
@@ -103,6 +103,6 @@ app.post('/users/:userId/cards', async (req, res) => {
 });
 
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log('app listening on port 3001');
 });
